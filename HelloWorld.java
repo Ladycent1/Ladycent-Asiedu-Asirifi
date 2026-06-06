40b2c18005badf1f00787dff98222097f2571cf7
Ladycent1 <ladycentasiedu0123@gmail.com>
2026-06-06 14:04:15 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,31 @@
 class HelloWorld {
  	public static void main(String[] args){
-System.out.println("Hello World!");
+ string fullName ="Ladycent Asirifi Asiedu";// 
+String:used for text like names
+ int age = 21;//int:used for whole numbers like age 
+ char gender ='F';//char: used for a single
+character 
+ double height =1.5;//double:used for decimal 
+numbers like height
+ double weight =65.1;//double:used for decimal
+numbers like weight 
+ boolean isStudent= true ;//boolean:used for true
+or false values 
+ long phoneNumber = 0559756245L;//long:used for 
+large whole numbers 
+ string level = "Level 200";//String:used for text
+ string nationality = "Ghanaian";//String:used for
+text
+
+  system.out.println("Full Name:"+ fullName);
+  system.out.println("Age:"+ age);
+  system.out.println("Gender:"+ gender);
+  system.out.println("Height:"+ "m");
+  system.out.println("Weight:"+ "kg");
+  system.out.println("Student Status:"+ isStudent);
+  system.out.println("Phone Number:"+ 
+phoneNumber);
+  system.out.println("Level:"+ level);
+  system.out.println("Nationality:"+ nationality);
 	}
 }
